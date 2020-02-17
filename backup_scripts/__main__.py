a5db76c90fb677944067f22c25c11f2d1e468523
Tom Ritchford <tom@swirly.com>
2020-02-17 19:54:42 +0100

    More backup scripts

--- backup_scripts/__main__.py
+++ backup_scripts/__main__.py
@@ -1,40 +1,56 @@
+from pathlib import Path
 import os
-from  queue import Empty, Queue
 import schedule
 import subprocess
+import sys
 import time
 
+RSYNC = 'rsync', '--archive', '-v', '--exclude=.git'
+GIT_STATUS = 'git', 'status', '--porcelain'
+GIT_COMMIT = 'git', 'commit', '-am'
 
-def main(source, target):
-    queue = Queue()
 
-    def call(*cmd):
-        return subprocess.call(cmd)
+def call(*args):
+    print('$', *args)
+    return subprocess.call(args)
+
+
+def check_output(*args):
+    return subprocess.check_output(args, encoding='utf-8')
+
+
+def changed_words(lines):
+    for line in lines:
+        is_version_control = ',' in line
+        if line.endswith('.txt') and not is_version_control:
+            action, filename = line.split(maxsplit=1)
+            yield Path(filename).stem
+
+
+def commit_all():
+    subprocess.call(('git', 'add', '.'))
+
+    out = check_output(*GIT_STATUS)
+    lines = [i.strip() for i in out.splitlines() if i.strip()]
+    if lines:
+        first = ', '.join(changed_words(lines))
+        if len(first) > 50:
+            first = first[:47] + '...'
+        lines.insert(0, first)
+        message = '\n'.join(lines or ['(None)']) + '\n'
+        call(*GIT_COMMIT, message)
+
+
+def main(source, target=None):
+    os.chdir(source)
 
     def rsync(period):
         t = os.path.join(target, period)
-        call('rsync', '--archive', source, t)
+        call(*RSYNC, source, t)
 
-    def git():
-        # Check if anything has changed
-        if call('git', 'diff-index', '--quiet', 'HEAD'):
-            queue.put(None)
-
-    def service_queue():
-        while True:
-            try:
-                queue.get(timeout=2)
-            except Empty:
-                continue
-            # Allow the wiki server time to write the RCS pages
-            time.sleep(0.1)
-
-
-    os.chdir(source)
-
-    schedule.every().second.do(git)
-    schedule.every().wednesday.at('5:32').do(rsync, 'weekly')
-    schedule.every().day.at('4:32').do(rsync, 'daily')
+    schedule.every().second.do(commit_all)
+    schedule.every().wednesday.at('05:32').do(rsync, 'weekly')
+    schedule.every().day.at('04:32').do(rsync, 'daily')
 
     while True:
         schedule.run_pending()
